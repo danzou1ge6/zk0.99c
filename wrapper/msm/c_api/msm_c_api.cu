801e3bc1f85997e3c55e4edd1e97ebf25c78bf8f
QiangXinwei <qiangxinwei@outlook.com>
2024-12-18 22:29:23 +0800

    MSM: add temp test code for batches from rust

--- wrapper/msm/c_api/msm_c_api.cu
+++ wrapper/msm/c_api/msm_c_api.cu
@@ -19,9 +19,11 @@ bool cuda_msm(unsigned int len, const unsigned int* scalers, const unsigned int*
     cudaHostRegister((void*)scalers, len * sizeof(Element), cudaHostRegisterDefault);
     cudaHostRegister((void*)points, len * sizeof(PointAffine), cudaHostRegisterDefault);
 
+    printf("Host register done\n");
+
     u32 *d_points, head;
 
-    using Config = msm::MsmConfig<255, 22, 3, 32, 2, 2, false>;
+    using Config = msm::MsmConfig<255, 22, 1, 8, 2, 2, false>;
 
     u32 *h_points[Config::n_precompute];
     h_points[0] = (u32*)points;
@@ -29,10 +31,28 @@ bool cuda_msm(unsigned int len, const unsigned int* scalers, const unsigned int*
         cudaHostAlloc(&h_points[i], len * sizeof(PointAffine), cudaHostAllocDefault);
     }
 
+    printf("host alloc points done\n");
+
+    int batch_size = 2;
+    const u32 **scaler_batches = new const u32*[batch_size];
+    scaler_batches[0] = (u32*)scalers;
+
+    for (int i = 1; i < batch_size; i++) {
+        cudaHostAlloc(&scaler_batches[i], len * sizeof(Element), cudaHostAllocDefault);
+        memcpy((void*)scaler_batches[i], (void*)scalers, len * sizeof(Element));
+    }
+
+    printf("host alloc scalers done\n");
+    Point *r = new Point[batch_size];
+
     cudaStream_t stream;
     cudaStreamCreate(&stream);
 
+    printf("start precompute\n");
+
     msm::precompute<Config>(h_points, len, stream);
+
+    printf("Precompute done\n");
 
     cudaEvent_t start, stop;
     float elapsedTime = 0.0;
@@ -41,12 +61,19 @@ bool cuda_msm(unsigned int len, const unsigned int* scalers, const unsigned int*
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    Point r;
-    msm::run<Config>(len, (u32*)scalers, const_cast<const u32 **>(h_points), r, false, false, d_points, head, stream);
+    msm::run<Config>(len, batch_size, scaler_batches, const_cast<const u32 **>(h_points), r, false, false, d_points, head, stream);
 
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&elapsedTime, start, stop);
+
+    printf("Run done\n");
+
+    for (int i = 1; i < batch_size; i++) {
+        cudaFreeHost((void*)scaler_batches[i]);
+    }
+
+    delete [] scaler_batches;
 
     cudaStreamDestroy(stream);
 
@@ -59,7 +86,7 @@ bool cuda_msm(unsigned int len, const unsigned int* scalers, const unsigned int*
     for (u32 i = 1; i < Config::n_precompute; i++) {
         cudaFreeHost(h_points[i]);
     }
-    auto r_affine = r.to_affine();
+    auto r_affine = r[0].to_affine();
 
     for(int i=0;i<Element::LIMBS;++i) {
         res[i] = r_affine.x.n.limbs[i];
