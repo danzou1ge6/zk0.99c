41ebcfae4a6bea5b62da25da5967d08079e218a7
QiangXinwei <qiangxinwei@outlook.com>
2025-01-23 11:27:08 +0800

    poly: improve kate_division

--- poly/src/kate_division.cuh
+++ poly/src/kate_division.cuh
@@ -66,9 +66,39 @@ __global__ void kate_kernel(Field *p, Field *q, Field *pow_b, Field b, u32 len_p
     q[seg_start + intra_seg_id] = q[seg_start + intra_seg_id] + (q[sum_id] * b + p[sum_id]) * pow_b[seg_len - 1 - intra_seg_id];
 }
 
+template <typename Field>
+__global__ void local_kate_kernel(Field *p, Field *q, Field *pow_b, Field b, u32 len_p, u32 max_deg) {
+    extern __shared__ Field shared[];
+    u32 index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index >= len_p / 2) return;
+    u32 seg_len = 1 << (max_deg - 1);
+    assert(seg_len >= threadIdx.x);
+    u32 inter_seg_id = index / seg_len;
+    u32 seg_start = inter_seg_id * seg_len * 2;
+    p += seg_start;
+    q += seg_start;
+    for (u32 i = threadIdx.x; i < seg_len * 2 * Field::LIMBS / 4; i += seg_len) {
+        reinterpret_cast<uint4*>(shared)[i] = uint4{0,0,0,0};
+    }
+    __syncthreads();
+    index = threadIdx.x;
+    for (u32 deg = 0; deg < max_deg; deg++) {
+        u32 seg_len = 1 << deg;
+        u32 inter_seg_id = index / seg_len;
+        u32 intra_seg_id = index % seg_len;
+        u32 seg_start = inter_seg_id * seg_len * 2;
+        u32 sum_id = seg_start + seg_len;
+        shared[seg_start + intra_seg_id] = shared[seg_start + intra_seg_id] + (shared[sum_id] * b + p[sum_id]) * pow_b[seg_len - 1 - intra_seg_id];
+        __syncthreads();
+    }
+    for (u32 i = threadIdx.x; i < seg_len * 2 * Field::LIMBS / 4; i += seg_len) {
+        reinterpret_cast<uint4*>(q)[i] = reinterpret_cast<uint4*>(shared)[i];
+    }
+}
+
 // assume len_p = 2^k
 template <typename Field>
-void gpu_kate_division(u32 log_p, Field *p, Field b, Field *q) {
+cudaError_t gpu_kate_division(u32 log_p, Field *p, Field b, Field *q) {
     u32 len_p = 1 << log_p;
     u32 *pow_b;
     cudaMalloc(&pow_b, len_p / 2 * Field::LIMBS * sizeof(u32));
@@ -79,11 +109,18 @@ void gpu_kate_division(u32 log_p, Field *p, Field b, Field *q) {
     cudaEventCreate(&end);
 
     cudaEventRecord(start);
-    cudaMemset(q, 0, len_p * Field::LIMBS * sizeof(u32));
+    u32 log_threads = 9;
+    u32 threads = 1 << log_threads;
+    u32 blocks = (len_p / 2 + threads - 1) / threads;
+
+    cudaFuncSetAttribute(local_kate_kernel<Field>, cudaFuncAttributeMaxDynamicSharedMemorySize, 2 * threads * sizeof(Field));
+
+    local_kate_kernel<Field> <<<blocks, threads, threads * 2 * sizeof(Field) >>> (reinterpret_cast<Field *>(p), reinterpret_cast<Field *>(q), reinterpret_cast<Field *>(pow_b), b, len_p, std::min(log_threads + 1, log_p));
+
+    PROPAGATE_CUDA_ERROR(cudaGetLastError());
     
-    for (int deg = 0; deg < log_p; deg++) {
-        u32 threads = 256;
-        u32 blocks = (len_p / 2 + threads - 1) / threads;
+    for (int deg = log_threads + 1; deg < log_p; deg++) {
+        
         kate_kernel<Field> <<<blocks, threads>>> (p, q, reinterpret_cast<Field *>(pow_b), b, len_p, deg);
     }
     cudaEventRecord(end);
@@ -94,6 +131,7 @@ void gpu_kate_division(u32 log_p, Field *p, Field b, Field *q) {
 
 
     cudaFree(pow_b);
+    return cudaSuccess;
 }
 
 } // namespace poly
