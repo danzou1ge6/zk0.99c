eb1ddc5a0a9f749d29e2268451666c984faedf7a
qiangxinwei <qiangxinwei@outlook.com>
2025-02-07 21:15:11 +0800

    ntt: possible faster impl

--- ntt/src/cooley_turkey_ntt.cuh
+++ ntt/src/cooley_turkey_ntt.cuh
@@ -1,6 +1,7 @@
 #pragma once
 #include <cassert>
 #include "ntt.cuh"
+#include "self_sort_in_place_ntt.cuh"
 
 namespace ntt {
 
@@ -123,6 +124,186 @@ namespace ntt {
 
         reinterpret_cast<Field*>(x)[idx0] = a;
         reinterpret_cast<Field*>(x)[idx1] = b;
+    }
+
+    template <typename Field>
+    __global__ void ntt_shfl_co (u32 * data, u32 log_len, u32 log_stride, u32 deg, u32 * roots) {
+        const static usize WORDS = Field::LIMBS;
+        static_assert(WORDS % 4 == 0);
+        using barrier = cuda::barrier<cuda::thread_scope_block>;
+        #pragma nv_diag_suppress static_var_with_dynamic_init
+        __shared__  barrier bar;
+
+        if (threadIdx.x == 0) {
+            init(&bar, blockDim.x); // Initialize the barrier with expected arrival count
+        }
+        __syncthreads();
+
+        extern __shared__ uint4 shfl[];
+
+        const u32 lid = threadIdx.x;
+        const u32 lsize = blockDim.x;
+        const u32 index = blockIdx.x;
+
+        const u32 lgp = log_stride - deg + 1;
+        const u32 end_stride = 1 << lgp; //stride of the last butterfly
+
+        // each segment is independent
+        const u32 segment_start = (index >> lgp) << (lgp + deg);
+        const u32 segment_id = index & (end_stride - 1);
+        
+        const u32 subblock_sz = 1 << (deg - 1); // # of neighbouring butterfly in the last round
+
+        data += ((u64)(segment_start + segment_id)) * WORDS; // use u64 to avoid overflow
+        
+        u32 io_id = lid & 1;            
+        u32 lid_start = lid - io_id;
+
+        Field a, b;
+
+        // Read data
+        if (WORDS == 8) {
+            u64 m_gpos = ((lid_start) & (subblock_sz - 1)) << lgp;
+            u64 n_gpos = ((lid_start + 1) & (subblock_sz - 1)) << lgp;
+
+            reinterpret_cast<uint4*>(a.n.limbs)[0] = reinterpret_cast<uint4*>(data + m_gpos * WORDS)[io_id];
+            reinterpret_cast<uint4*>(a.n.limbs)[1] = reinterpret_cast<uint4*>(data + n_gpos * WORDS)[io_id];
+
+            reinterpret_cast<uint4*>(b.n.limbs)[0] = reinterpret_cast<uint4*>(data + (m_gpos + (end_stride << (deg - 1))) * WORDS)[io_id];
+            reinterpret_cast<uint4*>(b.n.limbs)[1] = reinterpret_cast<uint4*>(data + (n_gpos + (end_stride << (deg - 1))) * WORDS)[io_id];
+
+            uint4 shfla = io_id ? reinterpret_cast<uint4*>(a.n.limbs)[0] : reinterpret_cast<uint4*>(a.n.limbs)[1];
+            uint4 shflb = io_id ? reinterpret_cast<uint4*>(b.n.limbs)[0] : reinterpret_cast<uint4*>(b.n.limbs)[1];
+
+            shfla.x = __shfl_xor_sync(0xffffffff, shfla.x, 1);
+            shfla.y = __shfl_xor_sync(0xffffffff, shfla.y, 1);
+            shfla.z = __shfl_xor_sync(0xffffffff, shfla.z, 1);
+            shfla.w = __shfl_xor_sync(0xffffffff, shfla.w, 1);
+            shflb.x = __shfl_xor_sync(0xffffffff, shflb.x, 1);
+            shflb.y = __shfl_xor_sync(0xffffffff, shflb.y, 1);
+            shflb.z = __shfl_xor_sync(0xffffffff, shflb.z, 1);
+            shflb.w = __shfl_xor_sync(0xffffffff, shflb.w, 1);
+
+            if (io_id) {
+                reinterpret_cast<uint4*>(a.n.limbs)[0] = shfla;
+                reinterpret_cast<uint4*>(b.n.limbs)[0] = shflb;
+            } else {
+                reinterpret_cast<uint4*>(a.n.limbs)[1] = shfla;
+                reinterpret_cast<uint4*>(b.n.limbs)[1] = shflb;
+            }
+        } else {
+            u64 gpos = (lid & (subblock_sz - 1)) << lgp;
+
+            a = Field::load(data + (gpos) * WORDS);
+            b = Field::load(data + (gpos + (end_stride << (deg - 1))) * WORDS);
+        }
+
+
+        barrier::arrival_token token = bar.arrive(); /* this thread arrives. Arrival does not block a thread */
+
+        const u32 pqshift = log_len - 1 - log_stride;
+
+        u32 i = 0;
+        for (; i + 6 < deg; i++) {
+            auto tmp = a;
+            a = a + b;
+            b = tmp - b;
+
+            u32 bit = subblock_sz >> i;
+            u64 di = (lid & (bit - 1)) * end_stride + segment_id;
+
+            if (di != 0) {
+                auto w = reinterpret_cast<Field*>(roots)[(di << i << pqshift)];
+                b = b * w;
+            }
+            
+            u32 lanemask = 1 << (deg - i - 2);
+            #pragma unroll
+            for (u32 j = 0; j < WORDS; j += 4) {
+                uint4 seg = uint4 {tmp.n.limbs[j], tmp.n.limbs[j + 1], tmp.n.limbs[j + 2], tmp.n.limbs[j + 3]};
+                u32 offset = j / 4 * blockDim.x + lid;
+                shfl[offset] = seg;
+            }
+            __syncthreads();
+            #pragma unroll
+            for (u32 j = 0; j < WORDS; j += 4) {
+                u32 offset = j / 4 * blockDim.x + (lid ^ lanemask);
+                uint4 seg = shfl[offset];
+                tmp.n.limbs[j] = seg.x;
+                tmp.n.limbs[j + 1] = seg.y;
+                tmp.n.limbs[j + 2] = seg.z;
+                tmp.n.limbs[j + 3] = seg.w;
+            }
+            if ((lid / lanemask) & 1) a = tmp;
+            else b = tmp;
+        }
+
+        for (; i < deg; i++) {
+            auto tmp = a;
+            a = a + b;
+            b = tmp - b;
+
+            u32 bit = subblock_sz >> i;
+            u64 di = (lid & (bit - 1)) * end_stride + segment_id;
+
+            if (di != 0) {
+                auto w = reinterpret_cast<Field*>(roots)[(di << i << pqshift)];
+                b = b * w;
+            }
+            
+            if (i + 1 < deg) {
+                u32 lanemask = 1 << (deg - i - 2);
+                tmp = ((lid / lanemask) & 1) ? a : b;
+
+                #pragma unroll
+                for (u32 j = 0; j < WORDS; j++) {
+                    tmp.n.limbs[j] = __shfl_xor_sync(0xffffffff, tmp.n.limbs[j], lanemask);
+                }
+
+                if ((lid / lanemask) & 1) a = tmp;
+                else b = tmp;
+            }
+        }
+        
+        bar.wait(std::move(token)); /* wait for all threads participating in the barrier to complete bar.arrive()*/
+
+        // Write back
+        if (WORDS == 8) {
+            u64 m_gpos = ((lid_start) & (subblock_sz - 1)) << (lgp + 1);
+            u64 n_gpos = ((lid_start + 1) & (subblock_sz - 1)) << (lgp + 1);
+            
+            uint4 shfla = io_id ? reinterpret_cast<uint4*>(a.n.limbs)[0] : reinterpret_cast<uint4*>(a.n.limbs)[1];
+            uint4 shflb = io_id ? reinterpret_cast<uint4*>(b.n.limbs)[0] : reinterpret_cast<uint4*>(b.n.limbs)[1];
+            
+            shfla.x = __shfl_xor_sync(0xffffffff, shfla.x, 1);
+            shfla.y = __shfl_xor_sync(0xffffffff, shfla.y, 1);
+            shfla.z = __shfl_xor_sync(0xffffffff, shfla.z, 1);
+            shfla.w = __shfl_xor_sync(0xffffffff, shfla.w, 1);
+            shflb.x = __shfl_xor_sync(0xffffffff, shflb.x, 1);
+            shflb.y = __shfl_xor_sync(0xffffffff, shflb.y, 1);
+            shflb.z = __shfl_xor_sync(0xffffffff, shflb.z, 1);
+            shflb.w = __shfl_xor_sync(0xffffffff, shflb.w, 1);
+            
+            if (io_id) {
+                reinterpret_cast<uint4*>(a.n.limbs)[0] = shfla;
+                reinterpret_cast<uint4*>(b.n.limbs)[0] = shflb;
+            } else {
+                reinterpret_cast<uint4*>(a.n.limbs)[1] = shfla;
+                reinterpret_cast<uint4*>(b.n.limbs)[1] = shflb;
+            }
+
+            reinterpret_cast<uint4*>(data + m_gpos * WORDS)[io_id] = reinterpret_cast<uint4*>(a.n.limbs)[0];
+            reinterpret_cast<uint4*>(data + n_gpos * WORDS)[io_id] = reinterpret_cast<uint4*>(a.n.limbs)[1];
+
+            reinterpret_cast<uint4*>(data + (m_gpos + end_stride) * WORDS)[io_id] = reinterpret_cast<uint4*>(b.n.limbs)[0];
+            reinterpret_cast<uint4*>(data + (n_gpos + end_stride) * WORDS)[io_id] = reinterpret_cast<uint4*>(b.n.limbs)[1];
+        } else {
+            u32 group_id = lid & (subblock_sz - 1);
+            u64 gpos = group_id << (lgp + 1);
+            
+            a.store(data + (gpos) * WORDS);
+            b.store(data + (gpos + end_stride) * WORDS);
+        }
     }
 
     template <typename Field>
@@ -424,11 +605,12 @@ namespace ntt {
                 dim3 block(block_sz);
                 dim3 grid(block_num);
 
-                auto kernel = ntt_shared_shfl <Field>;
+                auto kernel = ntt_shfl_co<Field>;// SSIP_NTT_stage1_warp_no_twiddle <Field, false>;
 
                 u32 shared_size = (sizeof(u32) * ((1 << deg) + 1) * WORDS) * group_num;
 
-                        
+                // kernel <<< grid, block, shared_size, stream >>>(x, log_len, log_stride, deg, 1 << (deg - 1), roots_d, zeta_d, start_n);
+
                 kernel <<< grid, block, shared_size, stream >>>(x, log_len, log_stride, deg, roots_d);
                         
                 CUDA_CHECK(cudaGetLastError());
